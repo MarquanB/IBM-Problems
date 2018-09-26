f2cec0cb1d301da16338abf9a4cb6bd19c7b49f8
Marquan Bowman <marquanb2017@gmail.com>
2018-09-25 20:21:41 -0400

    The last 2 IBM Problems

---
+++ TwoStrings.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        int a = s1.length();
+        int b = s2.length();
+        int c = 0;
+        
+        String results = "NO";
+        
+        for(int x = 0; x < a; x++){
+            for(int y = 0; y < b; y++){
+                if(s1.charAt(x) == s2.charAt(y)){
+                result = "YES";
+                break;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
